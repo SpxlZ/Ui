a6efcecc06e9c1c399b72d2efcd8b88c9824adbc
SpxlZ <146632787+SpxlZ@users.noreply.github.com>
2023-10-29 01:15:20 +0200

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "VortexLinx"
+	SaveManager.Folder = "SpxlZ"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
